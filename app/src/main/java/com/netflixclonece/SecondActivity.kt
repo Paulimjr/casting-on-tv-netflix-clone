538f210d889aea4428967fcf7b4badb45721e407
paulocamilo <paulo.camilo.sge@mango.com>
2021-05-09 13:29:23 +0200

    netflix close screen casting on TV Android

---
+++ app/src/main/java/com/netflixclonece/SecondActivity.kt
@@ -0,0 +1,15 @@
+package com.netflixclonece
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Created by Paulo Cesar on 18/04/2021.
+ */
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
